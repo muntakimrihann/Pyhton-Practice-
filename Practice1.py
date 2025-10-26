9d5bb277c490ea5cf62e9098e580e3d773b988bf
muntakimrihann <munthakimrihan7127@gmail.com>
2025-10-26 12:13:13 +0600

    first commit

---
+++ Practice1.py
@@ -0,0 +1,14 @@
+# My Biodata Program
+employee_name="Rihan"
+he="Sheikh Hasina"
+age=24
+cgpa=3.00
+#print("Muntakim " + " Mizan " + " Rihan ")
+print (employee_name+" is the new SDET(Software Development in Test)")
+print("Software Quality Assurance Engineer")
+print(he+" Dhaka theke palaise ")
+print(he+ " is currently",age, "years old")
+print("I am still unmarried, wanting to get married.But Rihan got problems with the Dandy")
+print(employee_name+" scored",cgpa,"cgpa in his Academics")
+
+
